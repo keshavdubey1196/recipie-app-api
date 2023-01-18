2543a4e3bbac4c9cf551d99bb98862d32005d906
keshav <keshavsharma1196@gmail.com>
2023-01-18 12:31:50 +0530

    Imlementing create new user API, adding serializerse, tests, adding views for the create user api, adding urls in User and App

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,7 @@
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/user/", include("user.urls")),
 ]
