e88cb2ca0fe0897d987c843865418b2d383d600b
keshav <keshavsharma1196@gmail.com>
2023-01-18 15:46:22 +0530

    Writing tests for token api(previous commit), implementing the token api

--- app/user/urls.py
+++ app/user/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
         "create/",
         views.CreateUserView.as_view(),
         name="create",
-    )
+    ),
+    path("token/", views.CreateTokenView.as_view(), name="token"),
 ]
