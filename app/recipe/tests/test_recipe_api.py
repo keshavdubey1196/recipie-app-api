a27ab683b8b02964f6f3a0c990b6756f9aebb284
keshav <keshavsharma1196@gmail.com>
2023-01-25 14:13:48 +0530

    Add additional tests/adding the test for fully updating a recipe

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -140,3 +140,27 @@ class PrivateRecipeAPITests(TestCase):
         self.assertEqual(recipe.title, payload["title"])
         self.assertEqual(recipe.link, original_link)
         self.assertEqual(recipe.user, self.user)
+
+    def test_full_update(self):
+        """Test full update of recipe."""
+        recipe = create_recipe(
+            user=self.user,
+            title="Sample recipe title",
+            link="http://example.com/recipe.pdf",
+            description="Sample recipe description",
+        )
+        payload = {
+            "title": "New recipe title",
+            "link": "http://example.com/new-recipe.pdf",
+            "description": "New recipe description",
+            "time_minutes": 18,
+            "price": Decimal("2.50"),
+        }
+        url = detail_url(recipe.id)
+        res = self.client.put(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        recipe.refresh_from_db()
+        for k, v in payload.items():
+            self.assertEqual(getattr(recipe, k), v)
+        self.assertEqual(recipe.user, self.user)
